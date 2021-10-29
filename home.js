60ac14923a828cc5314ee7ec678d19b21b507f0e
PulindaDissanayake <dpulinda@gmail.com>
2021-10-29 18:27:14 +0530

    final changes

--- home.js
+++ home.js
@@ -1,18 +1,18 @@
-var mybutton = document.querySelector("#myBtn");
+var mybutton = document.querySelector("#myButton");
 
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-function topFunction() {
+function scrollTopFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }

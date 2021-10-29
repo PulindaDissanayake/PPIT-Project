d6c6aac5b24e5c1a428eff2fc1b715be07b34892
PulindaDissanayake <dpulinda@gmail.com>
2021-10-29 21:31:12 +0530

    fixed nav

--- home.js
+++ home.js
@@ -1,14 +1,28 @@
 var mybutton = document.querySelector("#myButton");
+var navBar = document.querySelector(".navigation");
+var navBrand = document.querySelector(".navbar-brand");
+var navLinks = document.querySelectorAll(".nav-link")
+var i;
 
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
+    navBar.style.backgroundColor = "rgba(31, 50, 150, 1)";
+    navBrand.style.color = "white";
+    for (i = 0; i < navLinks.length; i++) {
+      navLinks[i].style.color = "white";
+    }
   } else {
     mybutton.style.display = "none";
+    navBar.style.backgroundColor = "#e1f3ff";
+    navBrand.style.color = "black";
+    for (i = 0; i < navLinks.length; i++) {
+      navLinks[i].style.color = "gray";
+    }
   }
 }
 
@@ -18,11 +32,11 @@ function scrollTopFunction() {
 }
 
 function toServices() {
-  var elmnt = document.getElementById("services");
-  elmnt.scrollIntoView();
+  var element = document.getElementById("services");
+  element.scrollIntoView();
 }
 
 function toAbout() {
-  var elmnt = document.getElementById("about");
-  elmnt.scrollIntoView();
+  var element = document.getElementById("about");
+  element.scrollIntoView();
 }
